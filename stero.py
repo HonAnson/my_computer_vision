348df082e783439d3ae6fbf06629cb81b1732b3f
HonAnson <hon.anson@gmail.com>
2024-06-13 23:04:02 +0100

    working on sift feature detection function

--- stero.py
+++ stero.py
@@ -7,7 +7,47 @@ path2 = 'path to iamge'
 ####
 
 
-image = cv2.imread(path1)
+img1 = cv2.imread(path1)
+img2 = cv2.imread(path2)
+
+
+
+# core idea: difference between two gaussian is close to normalized lapacian gaussina
+
+def getGaussianKernel(diameter, sigma):
+    if diameter%2 == 0:
+        raise ValueError("Diameter must be odd numbmer")
+        # Calculate the range for the kernel
+    
+    xx, yy = np.meshgrid(ax, ax)
+    kernel = np.exp(-(xx**2 + yy**2) / (2. * sigma**2))
+    
+    # apply normalization
+    kernel = kernel / np.sum(kernel)
+    return kernel
+
+
+
+def siftFeatures(img, scales):
+    # create scale space with different gaussian blur
+    kernels = []
+    for i in range(scales):
+        
+        pass
+
+
+
+
+
+
+    return
+
+
+
+
+
+
+
 image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 print(image_rgb.shape)
 
